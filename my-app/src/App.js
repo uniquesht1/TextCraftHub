def40e05f7a4f3e2ca8ffa545088daec0514b5a1
unique shrestha <unique@shrestha.com>
2023-11-19 23:13:47 +0545

    bootstrap is easy

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -22,7 +22,7 @@ function App() {
           <div className="collapse navbar-collapse" id="navbarSupportedContent">
             <ul className="navbar-nav me-auto mb-2 mb-lg-0">
               <li className="nav-item">
-                <a className="nav-link active" aria-current="page" href="https://www.google.com" target="_blank" rel="noopener noreferrer">
+                <a className="nav-link active" aria-current="page" href="https://www.google.com" target="_blank" rel="noopener noreferrer" >
                   Home
                 </a>
               </li>
