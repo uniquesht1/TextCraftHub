6fdbcebdeeb1965f9320e3ca1246fce4dc753989
unique shrestha <unique@shrestha.com>
2023-11-19 21:23:53 +0545

    had to do it all again

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,27 +1,26 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
   return (
     <>
-    
-    <h1>this is unique</h1>
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React With Unique
-        </a>
-      </header>
-    </div>
+      <h1>this is unique</h1>
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <p>
+            Edit <code>src/App.js</code> and save to reload.
+          </p>
+          <a
+            className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Learn React With Unique
+          </a>
+        </header>
+      </div>
     </>
   );
 }
