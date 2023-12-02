a75137229b518502da0a99953252da56bbd68831
unique shrestha <unique@shrestha.com>
2023-12-02 15:46:43 +0545

    hope its final commit

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import Navbar from "./components/Navbar";
 import TextForm from "./components/TextForm";
 import "./components/darkModeButton.css"; //do this free vayesi
 
-import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 function App() {
   const [mode, setMode] = useState("light");
